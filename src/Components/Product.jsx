0b519826a4832d90d67c0a371354dd774f730ce5
Shrushti <Shrushti>
2020-06-12 10:35:34 +0530

    product module api integration of categories color list and all product also done category by id product

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,15 +1,88 @@
-import React , { Component } from 'react';
+import React , {useContext, useState} from 'react';
+import SideBar from '../Components/SideBar'
+import '../Assets/css/product.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import {fetchCategory} from '../redux/action/categoriesAction';
+import {fetchColor} from '../redux/action/colorAction';
+import {fetchAllProduct} from '../redux/action/productCardAction';
+import {fetchCategoryId} from '../redux/action/categoriesAction';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {IoMdArrowDown , IoIosStar , IoMdArrowUp} from "react-icons/io";
+import ProductCard from '../Components/ProductCard';
+import { useCallback } from 'react';
 
 
 
 
-const Product = ()=> {
-    return (
-      <div>
-       <h1>Product component</h1>
-        </div>
-    )
+
+const Product = (props)=> {
+
+  const categoryList = useSelector((state)=> state.categoriesData.categories)
+  const colorList = useSelector((state)=> state.colorData.color);
+  const allProductList = useSelector((state) => state.productCardData.allProduct);
+  const CategoryIdList = useSelector((state) => state.categoriesData.categoryId);
+  const [ProductDataList , setProduct] = useState([]);
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    console.log('effect ')
+    dispatch(fetchCategory())
+   dispatch(fetchColor())
+   dispatch(fetchAllProduct())
+  
+  },[])
+// console.log('product data list', ProductDataList)
+  const getCategoryId = (id) => {
+    console.log(id)
+    dispatch(fetchCategoryId(id));
+         setProduct({
+          CategoryIdList
+         },
+         )   
   }
+  console.log('render')
+  return (
+  
+    <div>
+      <h5 className="sort">Sort By:<button className="star-btn" ><IoIosStar className="star-icon" /></button>
+        <button className="star-btn" >₹<IoMdArrowUp className="star-icon" /></button>
+        <button className="star-btn">₹<IoMdArrowDown className="star-icon" /></button>
+      </h5>
+        <Row className='row-product'>
+            <Col lg={3} className="col-sidebar">
+            <SideBar
+              colorList={colorList}
+              categoryList={categoryList}
+              getCategoryId={getCategoryId}
+              >
+            </SideBar>
+          </Col>
+          <Col>
+  <Row>
+      <Col lg={12} md={12} className="col-products">
+
+            {/* <ProductCard ProductData={ProductDataList}
+            /> */}
+          </Col>
+          </Row>
+          </Col>
+      
+        </Row>
+       
+        {/* <Pagination
+        hideFirstLastPages
+        prevPageText='prev'
+        nextPageText='next'
+    activePage={this.state.activePage}
+    itemsCountPerPage={5}
+    totalItemsCount={40} 
+    pageRangeDisplayed={5}
+    onChange={this.handlePageChange}
+  /> */}
+      </div>
+  )
+}
 
 
 export default Product
---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,71 @@
+import React  from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../Assets/css/productrating.css' ;
+import {Link} from 'react-router-dom'
+import {IoIosStarOutline , IoIosStar , IoIosStarHalf} from "react-icons/io";
+import axios from 'axios';
+import {ratingTotalCount } from '../Constants/enum';
+import {apiUrl } from '../Constants/api';
+
+
+import { Container } from 'react-bootstrap';
+
+
+const ProductCard = (props)=> {
+  
+      const {ProductData} = props;
+      return (
+        <div>
+          {
+            typeof ProductData == 'string' ?
+
+              <div>
+                <h2>{ProductData}</h2>
+              </div>
+              :
+              <Row>
+                { 
+                ProductData && ProductData.map((data) => {
+                  return (
+                    <Col lg={4}>
+                      <Card className="product-card-list">
+                        <Card.Img className="product-card-img" variant="top" src={"http://180.149.241.208:3022/" + data.product_image} />
+                        <Card.Body>
+                          <Card.Title className="product-card-link">
+                            {data.product_name}
+                          </Card.Title>
+                          <Card.Text className="prize">
+                            <p>Rs {data.product_cost}</p>
+                          <button className="add-cart-btn">Add To Cart</button >
+                          {
+                            ratingTotalCount.map((star_value, index) => {
+                              let product_rating = data.product_rating;
+                              return (
+                                product_rating - star_value > 0 ?
+                                  (<IoIosStar className="rating-star fill" key={star_value}>{product_rating}</IoIosStar>)
+                                  : ((product_rating - star_value) >= -0.5 && (product_rating - star_value) < 0) ?
+                                    (<IoIosStarHalf className="rating-star fill" key={star_value}></IoIosStarHalf>)
+                                    :
+                                    (<IoIosStarOutline className="rating-star" key={star_value}></IoIosStarOutline>)
+                              )
+                            })
+                          }
+                           </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  )
+                }
+                )
+                }
+              </Row>
+          }
+
+        </div>
+      )
+    }
+  
+  export default ProductCard
